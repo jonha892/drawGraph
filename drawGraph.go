ae6930069c1ea46d793021382d876b6b29c5fd4b
J0-nas <jonas@ottermoor.de>
2014-10-31 14:15:59 +0100

    added vertices, filling with color left

--- drawGraph.go
+++ drawGraph.go
@@ -3,41 +3,99 @@ package drawGraph
 import (
 	"bufio"
 	"code.google.com/p/draw2d/draw2d"
+	"errors"
 	"fmt"
 	"image"
+	"image/color"
 	"image/png"
 	"log"
 	"os"
 )
 
-func NewImage(width, height int) (image.Image, draw2d.GraphicContext) {
-	im := image.NewRGBA(image.Rect(0, 0, width, height))
-	gC := draw2d.NewGraphicContext(im)
+/*
+Package to draw 2D graphs (vertecies and lines)
+*/
 
-	gC.SetFillColor(image.White)
-	gC.SetStrokeColor(image.Black)
-
-	return im, gC
+type DrawObject struct {
+	im *image.RGBA
+	gC draw2d.GraphicContext
 }
 
-func SaveImage(name string, im image.Image) {
+var (
+	instance *DrawObject = nil
+	radius               = 5.0
+)
+
+func Instance() *DrawObject {
+	if instance == nil {
+		instance = new(DrawObject)
+		return instance
+	}
+	return instance
+}
+
+// Overrides current image with new image
+func (i *DrawObject) NewImage(width, height float64) {
+	i.im = image.NewRGBA(image.Rect(0, 0, int(width), int(height)))
+	i.gC = draw2d.NewGraphicContext(i.im)
+
+	white := color.NRGBA{0xff, 0xff, 0xff, 0xff}
+	i.gC.SetFillColor(white)
+	i.gC.SetStrokeColor(image.Black)
+	i.gC.MoveTo(0.0, 0.0)
+	i.gC.LineTo(width, 0.0)
+	i.gC.Stroke()
+	i.gC.MoveTo(width, 0.0)
+	i.gC.LineTo(width, height)
+	i.gC.Stroke()
+	i.gC.MoveTo(width, height)
+	i.gC.LineTo(0, height)
+	i.gC.Stroke()
+	i.gC.MoveTo(0.0, height)
+	i.gC.LineTo(0.0, 0.0)
+	i.gC.Stroke()
+	i.gC.Fill()
+	green := color.NRGBA{0x00, 0x66, 0x66, 0xff}
+	i.gC.SetFillColor(green)
+}
+
+func (i *DrawObject) SaveImage(name string) error {
+	if i.im == nil {
+		return errors.New("Image not available, not initialized!\n")
+	}
 	name = name + ".png"
 	file, err := os.Create(name)
 	if err != nil {
 		log.Println(err)
-		os.Exit(1)
+		return err
 	}
 	defer file.Close()
 	outWriter := bufio.NewWriter(file)
-	err = png.Encode(outWriter, im)
+	err = png.Encode(outWriter, i.im)
 	if err != nil {
 		log.Println(err)
-		os.Exit(1)
+		return err
 	}
 	err = outWriter.Flush()
 	if err != nil {
 		log.Println(err)
-		os.Exit(1)
+		return err
 	}
 	fmt.Println("Created " + name)
+	return nil
+}
+
+func (i *DrawObject) AddPoint(x, y float64) {
+	draw2d.Circle(i.gC, x, y, radius)
+	i.gC.Stroke()
+}
+
+func (i *DrawObject) PrintSomething() {
+	i.gC.MoveTo(10.0, 10.0)
+	i.gC.LineTo(100.0, 10.0)
+	i.gC.Stroke()
+	err := i.SaveImage("TestPath")
+	if err != nil {
+		log.Println(err)
+	}
 }
