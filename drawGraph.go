bb820eebfe3f2c1d37dcaba6ba1987ca82a196bc
jonha892 <jhalstrup@googlemail.com>
2022-06-17 12:36:07 +0200

    change dependency

--- drawGraph.go
+++ drawGraph.go
@@ -10,7 +10,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/llgcode/draw2d"
+	"github.com/fogleman/gg"
 )
 
 /*
@@ -19,7 +19,7 @@ Package to draw 2D graphs (vertecies and lines)
 
 type DrawObject struct {
 	im                  *image.RGBA
-	gC                  draw2d.GraphicContext
+	gC                  *gg.Context
 	maxHeight, maxWidth float64
 }
 
@@ -33,19 +33,21 @@ func Instance() *DrawObject {
 }
 
 // Overrides current image with new image
-func NewImage(width, height float64) *DrawObject {
+func NewImage(width, height int) *DrawObject {
 	i := new(DrawObject)
-	i.maxHeight = height
-	i.maxWidth = width
-	i.im = image.NewRGBA(image.Rect(0, 0, int(width), int(height)))
-	i.gC = draw2d.NewGraphicContext(i.im)
+	i.maxHeight = float64(height)
+	i.maxWidth = float64(width)
+	//i.im = image.NewRGBA(image.Rect(0, 0, int(width), int(height)))
+	gg.NewContext(width, height)
+	//i.gC = draw2dimg.NewGraphicContext(i.im)
 
 	white := color.NRGBA{0xff, 0xff, 0xff, 0xff}
-	i.gC.SetFillColor(white)
-	draw2d.Rect(i.gC, 0, 0, width, height)
+	i.gC.SetColor(white)
+	i.gC.DrawRectangle(0, 0, float64(width), float64(height))
+	// i.gC.ClearRect(0, 0, width, height)
 	i.gC.Fill()
 	green := color.NRGBA{0x00, 0x66, 0x66, 0xff}
-	i.gC.SetFillColor(green)
+	i.gC.SetColor(green)
 	i.gC.SetLineWidth(lineWidth)
 	return i
 }
@@ -84,7 +86,8 @@ func (i *DrawObject) AddPoint(x, y float64) error {
 		return errors.New("Coordinate > maximum")
 	}
 	//fmt.Println("Add point ", x, " ", y)
-	draw2d.Circle(i.gC, x, i.maxHeight-y, radius)
+	// draw2d.Circle(i.gC, x, i.maxHeight-y, radius)
+	i.gC.DrawCircle(x, i.maxHeight-y, radius)
 	i.gC.Fill()
 	return nil
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require github.com/llgcode/draw2d v0.0.0-20210904075650-80aa0a2a901d
 
 require (
+	github.com/fogleman/gg v1.3.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	golang.org/x/image v0.0.0-20180708004352-c73c2afc3b81 // indirect
+	golang.org/x/image v0.0.0-20220617043117-41969df76e82 // indirect
 )
